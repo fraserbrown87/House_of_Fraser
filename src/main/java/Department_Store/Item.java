c21590a754e5a69b1b20434951f7245951e2d6b5
fraserbrown87 <fraserbrown87@gmail.com>
2018-01-09 10:35:36 +0000

    created item class

---
+++ src/main/java/Department_Store/Item.java
@@ -0,0 +1,20 @@
+package Department_Store;
+
+public class Item {
+
+    private String category;
+    private String brand;
+    private String product;
+    private double sellPrice;
+    private double buyPrice;
+    private int stockLevel;
+
+    public Item{
+        this.category = category;
+        this.brand = brand;
+        this.product = product;
+        this.sellPrice = sellPrice;
+        this.buyPrice = buyPrice;
+        this.stockLevel = stockLevel;
+    }
+}
